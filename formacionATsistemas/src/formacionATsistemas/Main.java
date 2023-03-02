39b113f44abe6c46377ff6e82a6799889f05d6ae
SirEikir <cristoferpuerto@gmail.com>
2023-03-02 07:18:36 +0100

    Tomcat Server is ready

--- formacionATsistemas/src/formacionATsistemas/Main.java
+++ formacionATsistemas/src/formacionATsistemas/Main.java
@@ -3,10 +3,9 @@ package formacionATsistemas;
 public class Main {
 
 	public static void main(String[] args) {
-		int num1 = 2;
-		int num2 = 3;
+
 		System.out.println(" Hello world");
-		System.out.println(num1 + num2);
+		
 
 
 	}
